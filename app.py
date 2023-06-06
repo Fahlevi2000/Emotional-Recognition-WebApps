7386f794254be1114d5a1049199b2d9327236341
Mukhammad Fahlevi Ali Rafsanjani <64582353+Fahlevi20@users.noreply.github.com>
2023-06-06 09:17:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,44 @@
 import streamlit as st
-import numpy as np
-import cv2
 import keras
-from tensorflow.keras.preprocessing import image
+from PIL import Image
+import numpy as np
 
-st.title('Emotion Detection')
+st.title('Image Classification')
 
+# Load the trained model
 model = keras.models.load_model('model/my_model')
 model.load_weights('model/best_weights.h5')
-label_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happiness', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
-face_haar_cascade = cv2.CascadeClassifier('model/haarcascade_frontalface_default.xml')
 
-uploaded_file = st.file_uploader("Upload a photo", type=["jpg", "jpeg", "png"])
+# Define the class labels
+label_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happiness', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
+
+# Function to preprocess the image
+def preprocess_image(image):
+    img = image.resize((48, 48))  # Resize the image to the input size of the model
+    img = img.convert('L')  # Convert image to grayscale
+    img = np.array(img)  # Convert image to numpy array
+    img = np.expand_dims(img, axis=0)  # Add an extra dimension for batch size
+    img = img / 255.0  # Normalize the image
+    return img
+
+# Function to make predictions
+def make_prediction(image):
+    preprocessed_img = preprocess_image(image)
+    prediction = model.predict(preprocessed_img)
+    predicted_class = np.argmax(prediction)
+    class_label = label_dict[predicted_class]
+    return class_label
+
+# Streamlit app
+uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
-    # Read the uploaded image file
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    image = cv2.imdecode(file_bytes, 1)
+    # Read the image file and display it
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image', use_column_width=True)
 
-    # Convert image to grayscale
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Make prediction on the uploaded image
+    prediction = make_prediction(img)
 
-    # Detect faces in the image
-    faces = face_haar_cascade.detectMultiScale(image_gray, 1.3, 5)
-
-    # Process each detected face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = image_gray[y:y+h, x:x+w]
-        roi_gray = cv2.resize(roi_gray, (48, 48))
-        img_pixels = np.asarray(roi_gray)
-        img_pixels = np.expand_dims(img_pixels, axis=0)
-
-        predictions = model.predict(img_pixels)
-        emotion_label = np.argmax(predictions)
-
-        emotion_prediction = label_dict[emotion_label]
-
-        cv2.putText(image, emotion_prediction, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-
-        # Add additional information
-        cv2.putText(image, f"Emotion: {emotion_prediction}", (int(x), int(y + h + 30)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
-
-    # Display the processed image with bounding boxes and emotion labels
-    st.image(image, channels='BGR')
+    # Display the predicted class label
+    st.write('Predicted Emotion:', prediction)
